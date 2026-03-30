b945d9140021972b821cab47766a9b83296357aa
suraj-laxgan <surajdas11120@gmail.com>
2026-03-30 17:31:10 +0530

    fronend layout and design emplement

---
+++ resources/views/layout/frontend/footer.blade.php
@@ -0,0 +1,36 @@
+<footer id="footer" class="footer">
+
+    <div class="container">
+        <div class="copyright text-center ">
+            <p>© <span>Copyright</span> <strong class="px-1 sitename">EasyFolio</strong> <span>All Rights
+                    Reserved</span></p>
+        </div>
+        <div class="social-links d-flex justify-content-center">
+            <a href=""><i class="bi bi-twitter-x"></i></a>
+            <a href=""><i class="bi bi-facebook"></i></a>
+            <a href=""><i class="bi bi-instagram"></i></a>
+            <a href=""><i class="bi bi-linkedin"></i></a>
+        </div>
+        <div class="credits">
+            Designed by <a href="#">Suraj Dsa</a> | <a href="#">Suraj</a>
+        </div>
+    </div>
+
+</footer>
+
+<!-- Scroll Top -->
+<a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
+        class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="{{ asset('frontend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/vendor/php-email-form/validate.js') }}"></script>
+<script src="{{ asset('frontend/assets/vendor/aos/aos.js') }}"></script>
+<script src="{{ asset('frontend/assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
+<script src="{{ asset('frontend/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+
+<!-- Main JS File -->
+<script src="{{ asset('frontend/assets/js/main.js') }}"></script>
---
+++ resources/views/layout/frontend/services.blade.php
@@ -0,0 +1,68 @@
+ <section id="services" class="services section">
+
+     <!-- Section Title -->
+     <div class="container section-title" data-aos="fade-up">
+         <h2>Services</h2>
+         <div class="title-shape">
+             <svg viewBox="0 0 200 20" xmlns="http://www.w3.org/2000/svg">
+                 <path d="M 0,10 C 40,0 60,20 100,10 C 140,0 160,20 200,10" fill="none" stroke="currentColor"
+                     stroke-width="2"></path>
+             </svg>
+         </div>
+         <p>Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur
+             vel illum qui dolorem</p>
+     </div><!-- End Section Title -->
+
+     <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+         <div class="row align-items-center">
+             <div class="col-lg-4 mb-5 mb-lg-0">
+                 <h2 class="fw-bold mb-4 servies-title">Consectetur adipiscing elit sed do eiusmod tempor</h2>
+                 <p class="mb-4">Nulla metus metus ullamcorper vel tincidunt sed euismod nibh volutpat velit
+                     class aptent taciti sociosqu ad litora.</p>
+                 <a href="#" class="btn btn-outline-primary">See all services</a>
+             </div>
+             <div class="col-lg-8">
+                 <div class="row g-4">
+
+                     <div class="col-md-6" data-aos="fade-up" data-aos-delay="200">
+                         <div class="service-item">
+                             <i class="bi bi-activity icon"></i>
+                             <h3><a href="service-details.html">Eget nulla facilisi etiam</a></h3>
+                             <p>Vestibulum morbi blandit cursus risus at ultrices mi tempus imperdiet nulla.</p>
+                         </div>
+                     </div><!-- End Service Item -->
+
+                     <div class="col-md-6" data-aos="fade-up" data-aos-delay="300">
+                         <div class="service-item">
+                             <i class="bi bi-easel icon"></i>
+                             <h3><a href="service-details.html">Duis aute irure dolor</a></h3>
+                             <p>Auctor neque vitae tempus quam pellentesque nec nam aliquam sem et tortor.</p>
+                         </div>
+                     </div><!-- End Service Item -->
+
+                     <div class="col-md-6" data-aos="fade-up" data-aos-delay="400">
+                         <div class="service-item">
+                             <i class="bi bi-broadcast icon"></i>
+                             <h3><a href="service-details.html">Excepteur sint occaecat</a></h3>
+                             <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
+                                 praesentium.</p>
+                         </div>
+                     </div><!-- End Service Item -->
+
+                     <div class="col-md-6" data-aos="fade-up" data-aos-delay="500">
+                         <div class="service-item">
+                             <i class="bi bi-bounding-box-circles icon"></i>
+                             <h3><a href="service-details.html">Tempor incididunt ut labore</a></h3>
+                             <p>Ullamco laboris nisi ut aliquip ex ea commodo consequat duis aute irure dolor.
+                             </p>
+                         </div>
+                     </div><!-- End Service Item -->
+
+                 </div>
+             </div>
+         </div>
+
+     </div>
+
+ </section>
